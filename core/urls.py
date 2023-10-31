b1ece3265804b93144132c04657f17464646789b
Cristoferxo <cristofer@contratacion.com>
2023-10-30 22:21:31 -0300

    Colocacion de Login required

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from .views import home, nosotros, contacto, blog, formulario, registro, consulta, perfil, editarPerfil, noticias
 
 urlpatterns=[
@@ -9,7 +10,7 @@ urlpatterns=[
     path('formulario/', formulario, name="formulario"),
     path('registro/', registro, name="registro"),
     path('consulta/', consulta, name="consulta"),
-    path('perfil/', perfil, name="perfil"),
-    path('editarPerfil/<id>/', editarPerfil, name="editarPerfil"),
+    path('perfil/', login_required (perfil), name="perfil"),
+    path('editarPerfil/<id>/', login_required (editarPerfil), name="editarPerfil"),
     path('noticias/', noticias, name="noticias")
 ]
