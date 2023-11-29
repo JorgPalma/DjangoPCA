7ad3f3aaf9408b5061d0317f607cf594a19b47bf
Jagerist <diegofleeen@hotmail.com>
2023-11-28 22:44:32 -0300

    SABI QUE MAS CALLATE LOC CALLATE HMNO LA PULENTA

--- core/static/core/js/recomendador.js
+++ core/static/core/js/recomendador.js
@@ -3,8 +3,8 @@ var requestOptions = {
 };
 
 var params = {
-    api_token: 'jwH4Dreai77dYRlxV5rTP9E1vGbRhL3MzS0RgkFf',
-    search: 'apple',
+    api_token: 'I7GOkn9lxJmdUeNpxk3C4fMbJ8suQupV1mDPmaAi',
+    search: 'perros',
     language: 'es'
 };
 
@@ -18,6 +18,69 @@ fetch("https://api.thenewsapi.com/v1/news/all?" + query, requestOptions)
   .then(result => {
       // Accede al elemento DOM donde deseas mostrar los artículos
       var blogContainer = document.getElementById('blogContainer');
+
+      // Verifica si hay artículos en la respuesta
+      if (result.data && result.data.length > 0) {
+          // Recorre todos los artículos y crea elementos HTML para cada uno
+          result.data.forEach(article => {
+              // Filtra las noticias que no están en español
+              if (article.language !== 'es') {
+                  return;
+              }
+
+              var articleContainer = document.createElement('div');
+              articleContainer.classList.add('blog-post');
+
+              // Crea elementos para cada propiedad del artículo
+              var titleElement = document.createElement('h2');
+              titleElement.textContent = article.title;
+
+              var snippetElement = document.createElement('p');
+              snippetElement.textContent = article.snippet;
+
+              var imageElement = document.createElement('img');
+              imageElement.src = article.image_url;
+              imageElement.alt = article.title;
+
+              var sourceElement = document.createElement('p');
+              sourceElement.textContent = 'Source: ' + article.source;
+
+              var linkElement = document.createElement('a');
+              linkElement.href = article.url;
+              linkElement.textContent = 'Leer más';
+
+              // Agrega todos los elementos al contenedor del artículo
+              articleContainer.appendChild(titleElement);
+              articleContainer.appendChild(snippetElement);
+              articleContainer.appendChild(imageElement);
+              articleContainer.appendChild(sourceElement);
+              articleContainer.appendChild(linkElement);
+
+              // Agrega el contenedor del artículo al contenedor del blog
+              blogContainer.appendChild(articleContainer);
+          });
+      } else {
+          blogContainer.textContent = 'No se encontraron artículos';
+      }
+  })
+
+
+  var paramsa = {
+    api_token: 'I7GOkn9lxJmdUeNpxk3C4fMbJ8suQupV1mDPmaAi',
+    search: 'gatos',
+    language: 'es'
+};
+
+var esc = encodeURIComponent;
+var querya = Object.keys(paramsa)
+    .map(function(k) {return esc(k) + '=' + esc(paramsa[k]);})
+    .join('&');
+
+  fetch("https://api.thenewsapi.com/v1/news/all?" + querya, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+      // Accede al elemento DOM donde deseas mostrar los artículos
+      var blogContainer = document.getElementById('blogContainerHembra');
 
       // Verifica si hay artículos en la respuesta
       if (result.data && result.data.length > 0) {
