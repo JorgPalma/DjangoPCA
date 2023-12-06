d24ec89695f5ad02365893db0a85bdfc120bf585
Cristoferxo <cristofer@contratacion.com>
2023-12-06 15:28:22 -0300

    FILTRO

--- core/static/core/js/main.js
+++ core/static/core/js/main.js
@@ -3,7 +3,7 @@ let noti = document.getElementById('news');
 function Api_con_axios() {
     axios({
         method: 'GET',
-        url: 'https://gnews.io/api/v4/search?q=animal&country=es&apikey=623369ee5fa721da983d130d2a836c6b'
+        url: 'https://gnews.io/api/v4/search?q=perros&country=es&apikey=623369ee5fa721da983d130d2a836c6b'
     }).then(res => {
         let noticias = res.data.articles;
         console.log(noticias);
@@ -41,5 +41,26 @@ function Api_con_axios() {
         console.error('Error al cargar noticias:', error);
     });
 }
+
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Santiago,cl&appid=92193a0bddde1d5ebcc9787c63185f53&lang=sp')
+.then(response => response.json())
+.then(data => {
+  var tempValue = ((data['main']['temp'])-268.2).toFixed(1);
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Actualmente hay: "+descValue;
+  temp.innerHTML = "Temperatura - "+tempValue+"°C";
+
+})
+
 
 Api_con_axios();
